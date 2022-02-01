22e87012a977a9b284bcddcfea214ad917e51d98
Sean Nolan <sean@thenolans.com>
2022-02-01 10:31:44 -0800

    env

--- api/getSASToken/index.js
+++ api/getSASToken/index.js
@@ -1,13 +1,15 @@
-module.exports = async function (context, req) {
-    context.log('JavaScript HTTP trigger function processed a request.');
+module.exports = async function (ctx, req) {
+    context.log('entering ' + ctxt.executionContext.functionName + 
+                ' (' + ctx.executionContext.invocationId + ')');
 
-    const name = (req.query.name || (req.body && req.body.name));
-    const responseMessage = name
-        ? "Hello, " + name + ". This HTTP triggered function executed successfully."
-        : "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.";
+    var responseBody = 'Environment:\n\n';
 
-    context.res = {
+    for (const item : process.env) {
+        responseBody += item + '\n';
+    }
+    
+    ctx.res = {
         // status: 200, /* Defaults to 200 */
-        body: responseMessage
+        body: responseBody
     };
 }
