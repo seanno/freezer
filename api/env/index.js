2f9ffb947c57169ed3b0768cc45a928a7d5d9470
Sean Nolan <sean@thenolans.com>
2022-02-01 11:52:59 -0800

    move to env

--- api/env/index.js
+++ api/env/index.js
@@ -1,9 +1,17 @@
 module.exports = async function (ctx, req) {
+
     ctx.log('entering ' + ctx.executionContext.functionName + 
             ' (' + ctx.executionContext.invocationId + ')');
 
+    var responseBody = 'Environment:\n\n';
+
+    for (const item in process.env) {
+        responseBody = responseBody + item + ': ' + process.env[item] + '\n';
+    }
+    
     ctx.res = {
         // status: 200, /* Defaults to 200 */
-        body: 'nyi'
+        body: responseBody
     };
+
 }
